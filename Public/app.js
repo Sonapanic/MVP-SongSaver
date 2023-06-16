5f8a77b18d019aa0ecf6a95a058a068fe02ca7b1
Isaac Summers <monogator@gmail.com>
2023-06-16 13:53:31 -0700

    Testing get 1 route

--- Public/app.js
+++ Public/app.js
@@ -1,17 +1,25 @@
 const addBtn = $('#addBtn')
 const url = 'https://songsaver.onrender.com/song_info'
 
-$(addBtn).on('click', post)
+
+$(addBtn).on('click', getOne)
 
 function getAll(e) {
     e.preventDefault()
     fetchSongs(url)
 }
+function getOne(e) {
+    e.preventDefault()
+    fetchSongs(url`1`)
+}
 function post(e) {
     e.preventDefault()
     addSong(url)
 }
-
+function put(e) {
+    e.preventDefault()
+    editSong(url)
+}
 
 async function fetchSongs(url) {
     try {
@@ -43,8 +51,32 @@ async function addSong(url) {
         if (!response.ok) {
             throw new Error('Response not ok')
         }
-        const song = await response.json()
-        console.log(song)
+        const result = await response.json()
+        console.log(result)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function editSong(url) {
+    const modifiedSong = {
+        title: $('#titleInput').val(),
+        artist: $('#artistInput').val(),
+        genre: $('#genreInput').val()
+    }
+    try {
+        const response = fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(modifiedSong)
+        })
+        if (!response.ok) {
+            throw new Error('Response not ok')
+        }
+        const result = (await response).json()
+        console.log(result)
     } catch (err) {
         console.error(err)
     }

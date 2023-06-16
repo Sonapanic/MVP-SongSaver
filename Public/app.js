e81da12f8b7812f99df2ceeeaa42251767c9f644
Isaac Summers <monogator@gmail.com>
2023-06-16 11:16:22 -0700

    Added get all event listener

---
+++ Public/app.js
@@ -0,0 +1,19 @@
+const addBtn = $('#addBtn')
+
+// $(addBtn).click(post())
+
+// async function post() {
+//     const newSong = 
+//     fetch
+// }
+
+$(addBtn).on('click', getAll)
+
+async function getAll(e) {
+    e.preventDefault()
+    fetch("song_info/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Song Info:', data);
+    });
+  }
